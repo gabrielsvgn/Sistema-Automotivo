99f7e008689cc331e36ec3529235b9370b62f452
sevegnps-eng <sevegnps@gmail.com>
2026-04-25 15:29:31 -0300

    Enviando novos models Modelo Marca

--- classes/cliente.py
+++
@@ -1,7 +0,0 @@
-class Cliente:
-    def __init__(self, nome, numero, cpf, email):
-        self.id
-        self.nome = nome
-        self.numero = numero
-        self.cpf = cpf
-        self.email = email
