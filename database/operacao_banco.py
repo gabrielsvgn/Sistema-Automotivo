cad5d1d524d6aa80d7a0549b0f87667cb0e2ad14
sevegnps-eng <sevegnps@gmail.com>
2026-04-28 13:57:35 -0300

    Atualização README

--- database/operacao_banco.py
+++ database/operacao_banco.py
@@ -1,14 +1,14 @@
 from database.conexao import conectar
 from psycopg2 import sql
 
-# Banco Marca
+
 class BancoMarca:
     def __init__(self):
         self.conexao = conectar()
         self.cursor = self.conexao.cursor()
 
-    # Adicionar Marca
-    def adicionar_marca(self, marca):
+    
+    def post_car_brand(self, marca):
         try:
             self.cursor.execute("INSERT INTO marca (nome) VALUES (%s) RETURNING idmarca", (marca.nome,))
             id = self.cursor.fetchone()[0]
@@ -18,8 +18,8 @@ class BancoMarca:
             self.conexao.rollback()
             raise e
 
-    # Remover Marca
-    def remover_marca(self, idmarca):
+   
+    def delete_car_brand(self, idmarca):
         try:
             self.cursor.execute("DELETE FROM marca WHERE idmarca = %s", (idmarca,))
             self.conexao.commit()
@@ -27,8 +27,8 @@ class BancoMarca:
              self.conexao.rollback()
              raise e
 
-    # Atualizar Marca
-    def atualizar_marca(self, novo_nome, idmarca):
+    
+    def put_car_brand(self, novo_nome, idmarca):
         try:
             self.cursor.execute("UPDATE marca SET nome = %s WHERE idmarca = %s", (novo_nome.nome, idmarca,))
             self.conexao.commit()
@@ -36,8 +36,8 @@ class BancoMarca:
             self.conexao.rollback()
             raise e
         
-    # Listar Marcas
-    def listar_marcas(self):
+    
+    def get_car_brand(self):
         try:
             self.cursor.execute("SELECT * FROM marca")
             return self.cursor.fetchall()
@@ -45,14 +45,14 @@ class BancoMarca:
             self.conexao.rollback()
             raise e
 
-# Banco Modelo
+
 class BancoModelo:
     def __init__ (self):
         self.conexao = conectar()
         self.cursor = self.conexao.cursor()
 
-    # Adicionar Modelo
-    def adicionar_modelo(self, modelo):
+    
+    def post_car_model(self, modelo):
         try:
             self.cursor.execute("""INSERT INTO modelo (idmarca, nome) VALUES (%s, %s) RETURNING idmodelo""", (modelo.idmarca, modelo.nome,))
             id = self.cursor.fetchone()[0]
@@ -62,8 +62,29 @@ class BancoModelo:
             self.conexao.rollback()
             raise e
             
-    # Listar Todos
-    def listar_modelo(self):
+
+    
+    def put_car_model(self, idmodelo, novo_nome):
+        try:
+            self.cursor.execute("""UPDATE modelo SET nome = %s WHERE idmodelo = %s""", (novo_nome, idmodelo,))
+            self.conexao.commit()
+        except Exception as e:
+            self.conexao.rollback()
+            raise e
+
+    
+    def delete_car_model(self, idmodelo):
+        try:
+            self.cursor.execute("DELETE FROM modelo WHERE idmodelo = %s", (idmodelo,))
+            if self.cursor.rowcount == 0:
+                print("Modelo não encontrado")
+            self.conexao.commit()
+        except Exception as e:
+            self.conexao.rollback()
+            raise e
+        
+          
+    def get_car_model(self):
         try:
             self.cursor.execute("""SELECT
                                 mdl.nome,
@@ -79,28 +100,8 @@ class BancoModelo:
         except Exception as e:
             raise e
 
-
-    # Atualizar Modelos
-    def atualizar_modelo(self, idmodelo, novo_nome):
-        try:
-            self.cursor.execute("""UPDATE modelo SET nome = %s WHERE idmodelo = %s""", (novo_nome, idmodelo,))
-            self.conexao.commit()
-        except Exception as e:
-            self.conexao.rollback()
-            raise e
-
-    # Remover
-    def remover_modelo(self, idmodelo):
-        try:
-            self.cursor.execute("DELETE FROM modelo WHERE idmodelo = %s", (idmodelo,))
-            if self.cursor.rowcount == 0:
-                print("Modelo não encontrado")
-            self.conexao.commit()
-        except Exception as e:
-            self.conexao.rollback()
-            raise e
         
-# Banco Carro
+
 class BancoCarro:
     def __init__(self):
         self.conexao = conectar()
